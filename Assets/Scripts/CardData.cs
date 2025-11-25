c3bd7c3c489302ed4f814c225ee9caa7368e7e35
AyinPuppeteer <Ayin.Puppeteer@email.cn>
2025-11-25 22:04:12 +0800

    Update

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardData
+{
+    private string Name;
+    public string Name_ { get => Name; }
+
+    
+}
