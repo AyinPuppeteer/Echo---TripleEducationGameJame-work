25f4f171b9c3e577806ea56141e6c16f8579bd59
AyinPuppeteer <Ayin.Puppeteer@email.cn>
2025-11-25 23:57:45 +0800

    Update

---
+++ Assets/Scripts/System/CardEffect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ¿¨ÅÆÐ§¹û
+/// </summary>
+public abstract class CardEffect
+{
+    public abstract void OnWork();
+}
